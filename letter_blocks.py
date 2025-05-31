6e245aa8efd8f66aa5716823dde9f317268c60e9
KennethRMiller <kennymiller_7@hotmail.com>
2025-05-30 18:01:34 -0600

    adding combo finder

--- letter_blocks.py
+++ letter_blocks.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import itertools
-from nltk.corpus import words
 
 st.title("Letter Block Word Finder")
 st.write(
@@ -31,40 +30,64 @@ block_sets = st.slider("How many sets of blocks do you have?", min_value = 1, ma
 
 blocks = cubes * block_sets
 
-# Find words of length 4 (adjust word_length as needed)
-word_length = st.slider("Word Length", min_value=1, max_value=16, value=4)
-# Get the English dictionary words
-english_words = set(w.lower() for w in words.words() if len(w) == word_length)
+message = st.text_input("Type the message you're trying to spell",value="happy",max_chars=block_sets*16).replace(" ","")
+word_length = len(message)
 
-print(english_words)
+# def find_words(blocks, word_length):
+#     """
+#     Find words that can be spelled using the letters on the cubes.
+
+#     Args:
+#         cubes (list): List of cubes, each cube is a string of 4 letters.
+#         word_length (int): Length of words to find.
+
+#     Returns:
+#         list: List of words that can be spelled.
+#     """
+#     # Generate all possible combinations of letters
+#     combinations = itertools.product(*blocks)
+
+#     # Filter combinations that form valid English words
+#     words = []
+#     for combo in combinations:
+#         word = ''.join(combo).lower()
+#         if len(word) == word_length and word in english_words:
+#             words.append(word)
+
+#     return words
+
+def find_combinations(cubes, message):
+    message = message.upper()
+    combinations = []
+
+    def recursive_find(combinations, current_combination, remaining_word):
+        if not remaining_word:
+            combinations.append(current_combination[:])
+            return
+        for i, cube in enumerate(cubes):
+            if remaining_word[0] in cube:
+                current_combination.append((i + 1, remaining_word[0]))
+                recursive_find(combinations, current_combination, remaining_word[1:])
+                current_combination.pop()
+
+    recursive_find(combinations, [], message)
+    return combinations
+
+message = "HAPPY"
+combinations = find_combinations(cubes, message)
+
+combo_count = 0
+if st.button('Find Words'):
+    for i, combination in enumerate(combinations):
+        print(f"Combination {i + 1}:")
+        for cube_number, letter in combination:
+            print(f"Cube {cube_number}: {letter}")
+        combo_count += 1
+        if combo_count >= 10:
+            
+
+# if st.button('Find words'):
 
 
-def find_words(cubes, word_length):
-    """
-    Find words that can be spelled using the letters on the cubes.
-
-    Args:
-        cubes (list): List of cubes, each cube is a string of 4 letters.
-        word_length (int): Length of words to find.
-
-    Returns:
-        list: List of words that can be spelled.
-    """
-    # Generate all possible combinations of letters
-    combinations = itertools.product(*cubes)
-
-    # Filter combinations that form valid English words
-    words = []
-    for combo in combinations:
-        word = ''.join(combo).lower()
-        if len(word) == word_length and word in english_words:
-            words.append(word)
-
-    return words
-
-st.text_input("Type the message you're trying to spell",value="happy",max_chars=blocks)
-if st.button('Find words'):
-    words = find_words(blocks, word_length)
-
-    for word in set(words):  # Use set to remove duplicates
-        print(word)
+#     for word in set(words):  # Use set to remove duplicates
+#         print(word)
